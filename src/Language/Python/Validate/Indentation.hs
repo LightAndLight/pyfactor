cf7c36bd7a5900f9285f2f884c79ebc2261f054a
Isaac Elliott <isaace71295@gmail.com>
2018-02-22 17:52:17 +1000

    Added an indentation layer - prime

---
+++ src/Language/Python/Validate/Indentation.hs
@@ -0,0 +1,60 @@
+{-# language DataKinds #-}
+{-# language TypeOperators #-}
+module Language.Python.Validate.Indentation where
+
+import Control.Applicative
+import Control.Lens ((#))
+import Data.Coerce
+import Data.Type.Set
+import Data.Validate
+
+import Language.Python.Internal.Syntax
+import Language.Python.Validate.Indentation.Error
+
+data Indentation
+
+validateBlockIndentation
+  :: AsIndentationError e v a
+  => [(a, [Whitespace], Statement v a)]
+  -> Validate [e] [(a, [Whitespace], Statement (Nub (Indentation ': v)) a)]
+validateBlockIndentation = go Nothing
+  where
+    go _ [] = pure []
+    go a ((ann, ws, st):xs) =
+      case a of
+        Nothing -> liftA2 (:) ((,,) ann ws <$> validateStatementIndentation st) (go (Just ws) xs)
+        Just ws'
+          | ws == ws' -> liftA2 (:) ((,,) ann ws <$> validateStatementIndentation st) (go a xs)
+          | otherwise -> Failure [_BadIndent # (ws', ws, ann)] <*> (go a xs)
+
+validateExprIndentation
+  :: AsIndentationError e v a
+  => Expr v a
+  -> Validate [e] (Expr (Nub (Indentation ': v)) a)
+validateExprIndentation e = pure $ coerce e
+
+validateParamsIndentation
+  :: AsIndentationError e v a
+  => Params v a
+  -> Validate [e] (Params (Nub (Indentation ': v)) a)
+validateParamsIndentation e = pure $ coerce e
+
+validateArgsIndentation
+  :: AsIndentationError e v a
+  => Args v a
+  -> Validate [e] (Args (Nub (Indentation ': v)) a)
+validateArgsIndentation e = pure $ coerce e
+
+validateStatementIndentation
+  :: AsIndentationError e v a
+  => Statement v a
+  -> Validate [e] (Statement (Nub (Indentation ': v)) a)
+validateStatementIndentation (Fundef a name params body) =
+  Fundef a name <$>
+  validateParamsIndentation params <*>
+  validateBlockIndentation body
+validateStatementIndentation (If a expr body) =
+  If a <$>
+  validateExprIndentation expr <*>
+  validateBlockIndentation body
+validateStatementIndentation p = pure $ coerce p
